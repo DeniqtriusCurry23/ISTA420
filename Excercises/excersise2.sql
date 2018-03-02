407b85f27869c0950d8e9225ccbd2153b7386765
Deniqtrius Curry <Deniqtrius.curry@outlook.com>
2018-03-02 12:20:07 -0800

    New format

---
+++ Excercises/excersise2.sql
@@ -0,0 +1,16 @@
+.echo on
+.headers on
+
+-- Name : Lab2b.sql
+-- Author: Deniqtrius Curry
+-- Date : March 2nd, 2018
+
+select contacttitle || ' ' || contactname || '  
+ ' || companyname|| ' 
+ ' || address || '	
+ ' || city || ' ' || region || ' ' || postalcode || ' ' || country ||'
+'|| ' 
+' || ' 
+' || '
+'
+  from customers;
